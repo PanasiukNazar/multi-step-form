e0656d3586e671083fe2b1606920ef32e256e102
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2023-03-11 15:12:52 +0200

    split the store into several different files

---
+++ src/store/modules/form/state.js
@@ -0,0 +1,24 @@
+export default {
+    addOnsData: [
+        {
+            title: 'Online service',
+            text: 'Access to multiplayer games',
+            payment: 1,
+            selected: false
+        },
+        {
+            title: 'Larger storage',
+            text: 'Extra 1TB of cloud save',
+            payment: 2,
+            selected: false
+        },
+        {
+            title: 'Customizable Profile',
+            text: 'Custom theme on your profile',
+            payment: 2,
+            selected: false
+        },
+    ],
+
+    title: 'Hello'
+}
